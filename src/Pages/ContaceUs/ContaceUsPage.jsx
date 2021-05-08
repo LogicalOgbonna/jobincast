66cac74c1b99bb303b6b07dc64bca620acff2ec0
youfoundmalik <godswillugochukwu88@gmail.com>
2021-05-07 20:39:27 -0400

    update(cs update to contact us, privacy policy, terms and conditions pages)

--- src/Pages/ContaceUs/ContaceUsPage.jsx
+++ src/Pages/ContaceUs/ContaceUsPage.jsx
@@ -22,56 +22,56 @@ For other country,  send will submit message to the respective country email add
                         <RightColumn>
                             <p>Alternatively, you can send your email directly to the respective country of inquiry below.</p>
                             <ContactUsCards>
-                                <h5>Malaysia</h5>
-                                <p>11-1, Jalan Tasik Utama,</p>
-                                <p>Medan Niaga Tasik Damai,</p>
-                                <p>Sungei Besi,</p>
-                                <p>50470 Kuala Lumpur, Malaysia</p>
-                                <p>Email : desktop@jobincast.com.my</p>
+                                <h6>Malaysia</h6>
+                                <p className= "text-muted">11-1, Jalan Tasik Utama,</p>
+                                <p className= "text-muted">Medan Niaga Tasik Damai,</p>
+                                <p className= "text-muted">Sungei Besi,</p>
+                                <p className= "text-muted">50470 Kuala Lumpur, Malaysia</p>
+                                <p className= "text-muted">Email : desktop@jobincast.com.my</p>
                             </ContactUsCards>
                             <ContactUsCards>
-                                <h5>Indonesia</h5>
-                                <p>Belleza BSA, 1st Floor, Unit 106,</p>
-                                <p>Jl. Letjen Soepeno,</p>
-                                <p>Permata Hijau Kebayoran Lama,</p>
-                                <p>Jakarta Selatan 12210, Indonesia.</p>
-                                <p>Email : desktop@jobincast.co.id</p>
+                                <h6>Indonesia</h6>
+                                <p className= "text-muted">Belleza BSA, 1st Floor, Unit 106,</p>
+                                <p className= "text-muted">Jl. Letjen Soepeno,</p>
+                                <p className= "text-muted">Permata Hijau Kebayoran Lama,</p>
+                                <p className= "text-muted">Jakarta Selatan 12210, Indonesia.</p>
+                                <p className= "text-muted">Email : desktop@jobincast.co.id</p>
                             </ContactUsCards>
                             <ContactUsCards>
-                                <h5>Thailand</h5>
-                                <p>Suite 10.5, Golden Town Building,</p>
-                                <p>3rd Floor, Phayathai Road, Ratchathewi,</p>
-                                <p>10400 Bangkok, Thailand.</p>
-                                <p>Email :  desktop@jobincast.co.th</p>
+                                <h6>Thailand</h6>
+                                <p className= "text-muted">Suite 10.5, Golden Town Building,</p>
+                                <p className= "text-muted">3rd Floor, Phayathai Road, Ratchathewi,</p>
+                                <p className= "text-muted">10400 Bangkok, Thailand.</p>
+                                <p className= "text-muted">Email :  desktop@jobincast.co.th</p>
                             </ContactUsCards>
                             <ContactUsCards>
-                                <h5>Vietnam</h5>
-                                <p>Vietcombank Tower</p>
-                                <p>Level 4, No. 5 Mê Linh Square,</p>
-                                <p>Ben Nghe Ward, District 1,</p>
-                                <p>Ho Chi Minh City, Vietnam.</p>
-                                <p>Email :  desktop@jobincast.vn</p>
+                                <h6>Vietnam</h6>
+                                <p className= "text-muted">Vietcombank Tower</p>
+                                <p className= "text-muted">Level 4, No. 5 Mê Linh Square,</p>
+                                <p className= "text-muted">Ben Nghe Ward, District 1,</p>
+                                <p className= "text-muted">Ho Chi Minh City, Vietnam.</p>
+                                <p className= "text-muted">Email :  desktop@jobincast.vn</p>
                             </ContactUsCards>
                             <ContactUsCards>
-                                <h5>Singapore</h5>
-                                <p>Singapore Land Building</p>
-                                <p>5th Floor, 50 Raffles Place,</p>
-                                <p>Singapore 048623</p>
-                                <p>Email :  desktop@jobincast.com.sg</p>
+                                <h6>Singapore</h6>
+                                <p className= "text-muted">Singapore Land Building</p>
+                                <p className= "text-muted">5th Floor, 50 Raffles Place,</p>
+                                <p className= "text-muted">Singapore 048623</p>
+                                <p className= "text-muted">Email :  desktop@jobincast.com.sg</p>
                             </ContactUsCards>
                             <ContactUsCards>
-                                <h5>Philippines</h5>
-                                <p>LKG Tower,</p>
-                                <p>7th Floor, 6801 Ayala Avenue,</p>
-                                <p>Makati City 1226, Philippines.</p>
-                                <p>Email : desktop@jobincast.com.ph</p>
+                                <h6>Philippines</h6>
+                                <p className= "text-muted">LKG Tower,</p>
+                                <p className= "text-muted">7th Floor, 6801 Ayala Avenue,</p>
+                                <p className= "text-muted">Makati City 1226, Philippines.</p>
+                                <p className= "text-muted">Email : desktop@jobincast.com.ph</p>
                             </ContactUsCards>
                             <ContactUsCards>
-                                <h5>India</h5>
-                                <p>301, 3rd Floor, RG Trade Tower,</p>
-                                <p>Netaji Subhash Place, Pitam Pura,</p>
-                                <p>New Delhi-110034</p>
-                                <p>Email : desktop@jobincast.in</p>
+                                <h6>India</h6>
+                                <p className= "text-muted">301, 3rd Floor, RG Trade Tower,</p>
+                                <p className= "text-muted">Netaji Subhash Place, Pitam Pura,</p>
+                                <p className= "text-muted">New Delhi-110034</p>
+                                <p className= "text-muted">Email : desktop@jobincast.in</p>
                             </ContactUsCards>
                         </RightColumn>
                     </div>
--- src/Pages/Privacy/PrivacyPage.jsx
+++ src/Pages/Privacy/PrivacyPage.jsx
@@ -8,13 +8,13 @@ const PrivacyPage = () => {
             <div className="privacy-page desktop-layout">
                 <div className="row justify-content-center">
                     <div className="col-12 col-md-10 privacy-content">
-                        <h1>Privacy Policy</h1>
-
-                        <h3>Accounts and membership</h3>
+                        <h2>Privacy Policy</h2>
+                        <br></br>
+                        <h6>Accounts and membership</h6>
                         <p>You must be at least 18 years of age to use this Website. By using this Website and by agreeing to this Agreement you warrant and represent that you are at least 18 years of age. If you create an account on the Website, you are responsible for maintaining the security of your account and you are fully responsible for all activities that occur under the account and any other actions taken in connection with it. We may, but have no obligation to, monitor and review new accounts before you may sign in and use our Services.</p>
                         <p>Providing false contact information of any kind may result in the termination of your account. You must immediately notify us of any unauthorized uses of your account or any other breaches of security. We will not be liable for any acts or omissions by you, including any damages of any kind incurred as a result of such acts or omissions. We may suspend, disable, or delete your account (or any part thereof) if we determine that you have violated any provision of this Agreement or that your conduct or content would tend to damage our reputation and goodwill. If we delete your account for the foregoing reasons, you may not re-register for our Services. We may block your email address and Internet protocol address to prevent further registration.</p>
 
-                        <h3>User content</h3>
+                        <h6>User content</h6>
                         <p>
                             We do not own any data, information or material (“Content”) that you submit on the Website in the course of using the Service. You shall have sole responsibility for the accuracy, quality, integrity, legality, reliability, appropriateness, and intellectual property ownership or right to use of all submitted Content. We may, but have no obligation to, monitor and review Content on the Website submitted or created using our Services by you.
                     </p>
@@ -22,9 +22,9 @@ const PrivacyPage = () => {
                             Unless specifically permitted by you, your use of the Website does not grant us the license to use, reproduce, adapt, modify, publish or distribute the Content created by you or stored in your user account for commercial, marketing or any similar purpose. But you grant us permission to access, copy, distribute, store, transmit, reformat, display and perform the Content of your user account solely as required for the purpose of providing the Services to you. Without limiting any of those representations or warranties, we have the right, though not the obligation, to, in our own sole discretion, refuse or remove any Content that, in our reasonable opinion, violates any of our policies or is in any way harmful or objectionable.
                     </p>
 
-                        <h3>
+                        <h6>
                             Billing and payments
-                    </h3>
+                    </h6>
                         <p>
                             You shall pay all fees or charges to your account in accordance with the fees, charges, and billing terms in effect at the time a fee or charge is due and payable. If, in our judgment, your purchase constitutes a high-risk transaction, we will require you to provide us with a copy of your valid government-issued photo identification, and possibly a copy of a recent bank statement for the credit or debit card used for the purchase.
                     </p>
