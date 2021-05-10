12269f9a71cf412d43a98c9cacb8b804d15d9a54
youfoundmalik <godswillugochukwu88@gmail.com>
2021-05-10 18:08:07 -0400

    change of image on bloglist and dot css adjustment

--- src/components/Blocks/Blogs/BlogList.jsx
+++ src/components/Blocks/Blogs/BlogList.jsx
@@ -2,7 +2,7 @@ import './BlogList.less';
 
 import React from 'react';
 
-import { veggies } from '../../../assets/images';
+import { career_tip_3 } from '../../../assets/images/index';
 import { Pagination } from 'antd';
 import Blog from './Blog';
 import { NavLink } from 'react-router-dom';
@@ -14,7 +14,7 @@ const BlogList = ({ paginated }) => {
                 <div className="col-md-12">
                     <NavLink to="/blog/qwerty">
                         <Blog
-                            img={veggies}
+                            img={career_tip_3}
                             title="The 5 Essential Elements Your Resume Needs"
                             excert="There is no single document in a person’s life that is more obsessed over than the resumé. Mortgage papers, a Last Will and Testament, even divorce papers don’t have the same intensive review, page-for-page, than the notorious resumé..."
                             date="12th April, 2020"
