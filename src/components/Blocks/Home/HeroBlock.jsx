5f678cc44764071c629ba6adb3034d5c181c3294
Arinze Ogbonna <arinze.ogbonna@descasio.io>
2021-08-02 23:52:03 +0100

    fix: search and space between hero and featured companies

--- src/components/Blocks/Home/HeroBlock.jsx
+++ src/components/Blocks/Home/HeroBlock.jsx
@@ -9,11 +9,25 @@ const { Option } = Select;
 
 const HeroBlock = () => {
     const [search, setSearch] = useState({
-        jobCategory: '',
-        jobType: '',
-        jobCountry: '',
-        search: ''
+        // jobCategory: '',
+        // jobType: '',
+        // jobCountry: '',
+        // search: ''
     })
+
+    const toFormatted = () => {
+        let url = '/search?searchType=JOB&search=';
+        for (let key in search) {
+            if (key === "jobTitle") {
+                url = `${url}${key}==*${search[key]}*;`
+            } else {
+                url = `${url}${key}==${search[key]};`
+            }
+        }
+        return `${url.replace(/;$/, "")}&page=0&size=10`
+    }
+
+    const onSetSearch = (name, value) => setSearch({ ...search, [name]: value })
     return (
         <div className="hero-block">
             <div className="row">
@@ -23,17 +37,17 @@ const HeroBlock = () => {
                     <p className="hero-sub-heading">More than 10,000 trusted jobs are available on the website for candidates to send to their applications and exactly find their expected jobs</p>
 
                     <div className="hero-search-container">
-                        <Input onChange={e => setSearch({ ...search, search: e.target.value })} placeholder="Job Title" type="text" className="w-100" />
-                        <Select onChange={jobCategory => setSearch({ ...search, jobCategory })} className="w-100" placeholder="Job Category">
+                        <Input onChange={e => onSetSearch("jobTitle", e.target.value)} placeholder="Job Title" type="text" className="w-100" />
+                        <Select onChange={jobCategory => onSetSearch("jobCategory", jobCategory)} className="w-100" placeholder="Job Category">
                             {mappedJobCategories.map(({ label, value }) => <Option className="text-capitalize" value={value}>{label}</Option>)}
                         </Select>
-                        <Select onChange={jobType => setSearch({ ...search, jobType })} className="w-100" placeholder="Job Type">
+                        <Select onChange={jobType => onSetSearch("jobType", jobType)} className="w-100" placeholder="Job Type">
                             {mappedJobTypes.map(({ label, value }) => <Option className="text-capitalize" value={value}>{label}</Option>)}
                         </Select>
-                        <Select onChange={jobCountry => setSearch({ ...search, jobCountry })} className="w-100" placeholder="Job Location">
+                        <Select onChange={jobCountry => onSetSearch("jobCountry", jobCountry)} className="w-100" placeholder="Job Location">
                             {mappedCountries.map(({ label, value }) => <Option className="text-capitalize" value={value}>{label}</Option>)}
                         </Select>
-                        <Link to={`/search?searchType=JOB&search=jobCountry==${search.jobCountry};jobCategory==${search.jobCategory};jobType==${search.jobType};jobTitle=='*${search.search}*'&page=0&size=10`} className="w-50">Search</Link>
+                        <Link to={toFormatted()} className="w-50">Search</Link>
                     </div>
                 </div>
             </div>
